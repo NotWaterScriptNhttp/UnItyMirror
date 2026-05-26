425dcc9fceb03fa23c5139837277e4b6137c8ffe
NotWaterScriptNhttp <JustNotKokot.cc.pub.priv@seznam.cz>
2026-05-26 18:00:21 +0200

    Removed meta files

---
+++ Assets/Mirror/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("UnItyMirror")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("997-13854")]
+[assembly: AssemblyProduct("UnItyMirror")]
+[assembly: AssemblyCopyright("Copyright © 997-13854 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("d7948180-00da-4ecd-bf3d-4bd10e25b11a")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
